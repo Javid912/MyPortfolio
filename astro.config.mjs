e39bca8fff2392d871492f23f801f23944c0e323
Javid912 <56196165+Javid912@users.noreply.github.com>
2025-01-04 12:28:31 +0100

    Fix asset paths and rebuild site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://javid912.github.io',
   base: '/MyPortfolio',
+  build: {
+    assets: '_astro'
+  },
+  outDir: './dist',
+  publicDir: './public',
 });
